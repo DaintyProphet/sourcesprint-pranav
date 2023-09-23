f29c1a4076c639c272c44b75a6aed1c5ba794f12
DaintyProphet <145823434+DaintyProphet@users.noreply.github.com>
2023-09-23 14:51:34 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,8 @@
+X = eval(input("Enter the first number : "))
+Y = eval(input("Enter the second number : "))
+sum = X + Y
+difference = X - Y
+
+print(sum)
+print(difference)
+print("This is a calculator")
